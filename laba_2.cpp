a6e9e5f230fb874b9505c8a94e53be1a1b9f0e49
SurovtsevaVictoriia <78654989+SurovtsevaVictoriia@users.noreply.github.com>
2021-10-28 23:36:15 +0300

    finished
    does the separate splines as well

--- laba_2.cpp
+++ laba_2.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <fstream>
 #include <locale>
+#include<string>
 
 
 void print_as_poly(std::vector<double> vec) {
@@ -16,6 +17,7 @@ void print_as_poly(std::vector<double> vec) {
 
 	std::cout << 0 << std::endl;
 }
+
 void print(std::vector<double> vec) {
 
 	
@@ -34,6 +36,39 @@ double power(double x, int n) {
 	return value;
 }
 
+int factorial(int n) {
+
+	switch (n)
+	{
+	case 1:
+		return 1;
+		break;
+	case 2:
+		return 2;
+		break;
+	case 3:
+		return 6;
+		break;
+	case 4:
+		return 24;
+		break;
+	case 5:
+		return 120;
+		break;
+	case 6:
+		return 720;
+		break;
+
+	}
+
+
+	int a = 1;
+	for (int i = 1; i <= n; i++) {
+		a = a * i;
+	}
+	return a;
+}
+
 
 double get_value(std::vector<double> coef_, double x) {//from array
 	double value = 0;
@@ -41,6 +76,20 @@ double get_value(std::vector<double> coef_, double x) {//from array
 		value += power(x, coef_.size() - 1 - i) * coef_[i];
 	}
 	return value;
+}
+
+auto get_derivative(std::vector<double> &coef_) {//derivative from array
+
+	
+	int size = coef_.size() - 1;
+	std::vector<double> array_(size);
+
+	for (int i = 0; i < size; i++) {
+		array_[i] = coef_[i] * double(factorial(coef_.size() - 1 - i)) / double(factorial(coef_.size() - 1 - i - 1));
+	}
+
+
+	return array_;
 }
 
 
@@ -58,10 +107,13 @@ public:
 	
 	}
 
-	std::vector<double> new_poly;
+	
 	std::vector<std::vector<double>> residue;//(x-x0)(x-x1)(x-x2)
-	std::vector<double> coefs;
-	std::vector<double> final_coefs;
+	std::vector<double> coefs;//b
+	std::vector<double> final_coefs;//extrapolating polynom
+	std::vector<double> derivative;//derivative
+	std::vector<std::vector<double>> extra_coefs;//on two dots[[0123][0123]]...
+
 	void calculate_residue() {//works
 
 		residue.push_back(std::vector<double>{ 1, -values[0][0] });		
@@ -79,14 +131,11 @@ public:
 		}
 	}
 
-
 	void calculate_coefs() {//recurrent shit for b
 		for (int i = 0; i < N + 1; i++) {
 			coefs.push_back(ff(i, i));
 		}
-	}
-
-	
+	}	
 	//would be more effective if i would just calculate buffer vectors but idc
 	double ff(int i, int j) {
 		if (i == 0) { 
@@ -129,6 +178,58 @@ public:
 		}
 		a += coefs[0];
 		final_coefs.push_back(a);
+
+		
+	}
+
+	void calculate_extra_coefs() {
+		std::cout << "calculating extra coefs\n";
+		for (int i = 0; i < N; i++) {//number of intervals
+			std::vector<double> buffer;
+
+			for (int j = 0; j < 4; j++) {
+				std::cout << "a: i = " << i << " j = " << j << " \t" << a(i, j) << "\n";
+				buffer.push_back(a(i, j));
+			}
+
+			extra_coefs.push_back(buffer);
+			
+			print(buffer);
+		}
+	}
+
+	double a(int i, int j) {
+		std::vector<double> x = values[0];
+		std::vector<double> y = values[1];
+		switch (j) {
+			case 0:
+				return (get_value(derivative, x[i + 1]) * (x[i + 1] - x[i])
+					- 2 * (y[i + 1] - y[i])
+					+ get_value(derivative, x[i]) * (x[i + 1] - x[i]))
+					/ power(x[i + 1] - x[i], 3);
+				break;
+
+			case 1:
+				return  (-get_value(derivative, x[i + 1]) * (x[i + 1] - x[i]) * (x[i + 1] + 2 * x[i])
+					+ 3 * (y[i + 1] - y[i]) * (x[i + 1] + x[i])
+					- get_value(derivative, x[i]) * (x[i + 1] - x[i]) * (x[i] + 2 * x[i+1]))
+					/ power(x[i + 1] - x[i], 3);
+				break;
+
+			case 2:
+				return  (get_value(derivative, x[i + 1]) * x[i] * (2 * x[i + 1] + x[i]) * (x[i + 1] - x[i])
+					- 6 * (y[i + 1] - y[i]) * x[i] * x[i + 1]
+					+ get_value(derivative, x[i]) * x[i + 1] * (x[i + 1] + 2 * x[i]) * (x[i + 1] - x[i]))
+					/ power(x[i + 1] - x[i], 3);
+				break;
+			case 3:
+				return (-get_value(derivative, x[i + 1]) * x[i] * x[i] * x[i + 1] * (x[i + 1] - x[i])
+					+ y[i + 1] * x[i] * x[i] * (3 * x[i + 1] - x[i])
+					+ y[i] * x[i + 1] * x[i + 1] * (x[i + 1] - 3 * x[i])
+					- get_value(derivative, x[i]) * x[i] * x[i + 1] * x[i + 1] * (x[i + 1] - x[i]))
+					/ power(x[i + 1] - x[i], 3);
+				break;
+		}
 	}
 };
 
@@ -138,7 +239,7 @@ void get_dots(Spline s) {
 
 	using namespace std;
 	
-	cout << "placing dots";
+	cout << "placing dots\n";
 
 	string outFile = "out.csv";
 	string inFile = "in.csv";
@@ -168,33 +269,76 @@ void get_dots(Spline s) {
 	}
 
 	out << "xx;yy;\n";
-	double start = s.values[0][0] - 1;
-	double end = s.values[0][s.N] + 1;
-	double step = 0.005;
+	double start = s.values[0][0] ;
+	double end = s.values[0][s.N] ;
+	double step = 0.05;
 
-	for (auto i = start; i <= (end / step); ++i) {
+	std::cout << "start: " << start << " \t end: " << end << "\n";
+	for (int i = (start / step) - 1; i <= (end / step) + 1; ++i) {
+		std::cout << i << " ";
 		out << i * step << ";" << get_value(s.final_coefs, i * step) << ";" << "\n";
 		
 	}
+	std::cout << "\n";
 	out.close();
 
 	
 }
 
+void get_more_dots(Spline s) {
 
-void do_the_thing(std::vector<double> &x, std::vector<double>& y,  Spline & s) {
+	using namespace std;
+	ofstream out;
+
+	for (int i = 0; i < s.N; i++) {
+
+		string outFile = "out" + to_string(i) + ".csv";
+		out.open(outFile);
+
+		out << "xxx" + to_string(i) + ";yyy" + to_string(i)+ ";\n";
+		double step = 0.05;
+		double start = s.values[0][i];
+		double end = s.values[0][i + 1];
+		std::cout << "start: " << start << " \t end: " << end << "\n";
+
+
+		for (int j = (start / step)  -1 ; j <= (end / step) + 1 ; ++j) {
+			std::cout << j << " ";
+			out << j * step << ";" << get_value(s.extra_coefs[i], j * step) << ";" << "\n";
+			
+		}
+		std::cout << "\n";
+		out.close();
+	}
+
+	//out.close();
+	
+
+
+}
+void do_the_thing(Spline & s) {//on all
 	
 	//сначала делаем по всем, потом по трем соседним
 	s.calculate_residue();
 	s.calculate_coefs();
-
 	s.calculate_coefs_final();
-
-	print_as_poly(s.residue[2]);
+	
+	//print_as_poly(s.residue[2]);
 	print(s.coefs);
 	print_as_poly(s.final_coefs);
 
 	get_dots(s);
+}
+
+void do_another_thing(Spline & s) {//on two 
+
+	s.derivative = get_derivative(s.final_coefs);
+	print_as_poly(s.derivative);
+	s.calculate_extra_coefs();
+
+	get_more_dots(s);
+
+
 }
 
 int main() {
@@ -213,7 +357,9 @@ int main() {
 	std::vector<double> y = { 0.000038, 0.00052, 0.00254, 0.01013, 0.03636, 0.11699 };
 
 	Spline s = Spline(x, y);
-	do_the_thing(x, y, s);
+
+	do_the_thing(s);
+	do_another_thing(s);
 	
 	std::cout << "s";
 
