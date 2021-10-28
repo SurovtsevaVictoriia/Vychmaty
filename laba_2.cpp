d4b3ba1db9333dbe9ee875f9672db6e128e4c812
SurovtsevaVictoriia <78654989+SurovtsevaVictoriia@users.noreply.github.com>
2021-10-28 14:52:46 +0300

    works
    succesfully builds polynom of required power

--- laba_2.cpp
+++ laba_2.cpp
@@ -31,15 +31,16 @@ public:
 	Spline(std::vector<double> x_values_, std::vector<double> y_values_) {
 		
 		N = x_values_.size() - 1;
-		for (int i = 0; i < N + 1; i++) {
+		
 			values.push_back(x_values_);
 			values.push_back(y_values_);
-		}
+	
 	}
 
 	std::vector<double> new_poly;
 	std::vector<std::vector<double>> residue;//(x-x0)(x-x1)(x-x2)
 	std::vector<double> coefs;
+	std::vector<double> final_coefs;
 	void calculate_residue() {//works
 
 		residue.push_back(std::vector<double>{ 1, -values[0][0] });		
@@ -58,24 +59,56 @@ public:
 	}
 
 
-	void calculate_coefs() {//recurrent shit
-		for (int i = 0; i < N; i++) {
+	void calculate_coefs() {//recurrent shit for b
+		for (int i = 0; i < N + 1; i++) {
 			coefs.push_back(ff(i, i));
 		}
 	}
 
 	
+	//would be more effective if i would just calculate buffer vectors but idc
 	double ff(int i, int j) {
-		if (i == 0) { return values[1][0]; }
+		if (i == 0) { 
+			double a = values[1][0];
+			std::cout << " i = " << i << " j = " << j << "\t" << a << "\n";
+			return values[1][0]; 
+		}
 		else if (i == 1) {
-			return (values[1][1] - values[1][0]) / values[0][1] - values[0][0];
+			double a = (values[1][j] - values[1][j - 1]) / (values[0][j] - values[0][j - 1]);
+			std::cout <<" i = 1, j = " << j << "\t" << a << "\n";
+			return a;
 		}
 		else {
-			return (ff(i - 1, j) - ff(i - 1, j - 1)) / values[0][j] - values[0][j - i];
+			double a = (ff(i - 1, j) - ff(i - 1, j - 1)) /( values[0][j] - values[0][j - i]);
+			std::cout << " i = " << i << " j = " << j << "\t" << a << "\n";
+			return a ;
 		}
-
+		
 	}
 
+	void calculate_coefs_final() {//for 
+
+		for (int i = 0; i < N; i++) {//
+
+			double a = 0;
+			for (int j = 0; j <= i; j++) {
+
+				a += residue[N - j - 1][i - j] * coefs[N - j];
+								
+			}
+
+			final_coefs.push_back(a);
+		}
+
+		double a = 0;
+		for (int j = 0; j < N; j++) {
+
+			a += residue[N - j - 1][N - j] * coefs[N - j];
+
+		}
+		a += coefs[0];
+		final_coefs.push_back(a);
+	}
 };
 
 
@@ -87,16 +120,24 @@ int main() {
 
 	std::vector<double> x_test = { 1, 2, 4 };
 	std::vector<double> y_test = { 1, 2, 3 };
+
+	std::vector<double> x_test2 = { 0, 1, 2, 3, 4 };
+	std::vector<double> y_test2 = { 1, 5, 33, 109, 257 };
 	
 
 	std::vector<double> x = { 0.17453, 0.5236, 0.87267, 1.22173, 1.5708, 1.91986 };
 	std::vector<double> y = { 0.000038, 0.00052, 0.00254, 0.01013, 0.03636, 0.11699 };
 	
-	Spline s = Spline(x_test, y_test);
+	Spline s = Spline(x, y);
 	//сначала делаем по всем, потом по трем соседним
 	s.calculate_residue();
+	s.calculate_coefs();
+
+	s.calculate_coefs_final();
+
 	print_as_poly(s.residue[2]);
 	print(s.coefs);
-	std::cout << "works";
+	print_as_poly(s.final_coefs);
+	std::cout << "s";
 
 }
